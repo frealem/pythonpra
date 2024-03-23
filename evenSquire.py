a0486c94635718f9195cb6f8338883cb06cf2d34
frealem <frealemtekalign156@gmail.com>
2024-03-23 19:57:53 +0300

    even

--- evenSquire.py
+++ evenSquire.py
@@ -1,6 +1,5 @@
 x=int(input('enter even number'))
-
-if (x%2!=0):
-    print("please enter even try again!")
-else:
-    print("the power is: ",x**2)    
+while(x%2!=0):
+    print("please try again!") 
+   
+print("the power : ",x**2)
