4ab821c2513e7ee1f95015887a42237dccc7d0af
frealem <frealemtekalign156@gmail.com>
2024-04-01 16:10:25 +0300

    dict2

--- dict.py
+++ dict.py
@@ -1,5 +1,15 @@
-# this about dictionary
-dict={"x":30,"y":40,"z":50}
-print(dict)
-dict["z"]=60
-print(dict)
+# # this about dictionary
+# dict={"x":30,"y":40,"z":50}
+# print(dict)
+# dict["z"]=60
+# print(dict)
+
+# # to access the value using key get()
+# print(dict.get("x"))
+# # another form of dict
+dict2=dict([(1,"jave"),(2,"javawa"),(3,"javeye")])
+
+print(dict2)
+
+# if i want access javawa
+print(dict2[2])
