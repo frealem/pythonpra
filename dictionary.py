e59077614d36d83aaef15f73ac7ef4c509b1f803
frealem <frealemtekalign156@gmail.com>
2024-04-09 08:56:07 +0300

    last dict

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+# len()
+diction=dict({"val1":1,"val2":2,"val3":3,})
+
+legth=len(diction)
+print("this is the length of dictionary:",legth)
+comp=str(diction)
+print("this is the components of dict:",comp)
+typee=type(diction)
+print("this is the type:",typee)
